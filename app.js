1d350486b1aa40a1e644badf1fe8a13951c301ea
james reantillo <james.reantillo@gmail.com>
2021-02-13 00:33:39 -0800

    update background

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //Global Selection and Variables
 const colorDivs = document.querySelectorAll('.color');
 const generateBtn = document.querySelector('.generate');
-const sliders = document.querySelectorAll('.input[type="range"]');
+const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
 let initialColors;
+
+sliders.forEach((slider) => {
+  slider.addEventListener('input', hslControls);
+});
 
 //Functions
 
@@ -18,12 +22,13 @@ function randomColors() {
     const hexText = div.children[0];
     const randomColor = generateHex();
 
-    //Add the color to the background
+    //Add the color to the bg
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
+
     //Check for contrast
-    checkContrast(randomColor, hexText);
-    //Initial colorize sliders
+    checkTextContrast(randomColor, hexText);
+    //Initial Colorize Sliders
     const color = chroma(randomColor);
     const sliders = div.querySelectorAll('.sliders input');
     const hue = sliders[0];
@@ -34,7 +39,7 @@ function randomColors() {
   });
 }
 
-function checkContrast(color, text) {
+function checkTextContrast(color, text) {
   const luminance = chroma(color).luminance();
   if (luminance > 0.5) {
     text.style.color = 'black';
@@ -44,6 +49,7 @@ function checkContrast(color, text) {
 }
 
 function colorizeSliders(color, hue, brightness, saturation) {
+  //Scale Saturation
   const noSat = color.set('hsl.s', 0);
   const fullSat = color.set('hsl.s', 1);
   const scaleSat = chroma.scale([noSat, color, fullSat]);
@@ -60,4 +66,29 @@ function colorizeSliders(color, hue, brightness, saturation) {
   )},${scaleBright(0.5)} ,${scaleBright(1)})`;
   hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
+
+function hslControls(e) {
+  const index =
+    e.target.getAttribute('data-bright') ||
+    e.target.getAttribute('data-hue') ||
+    e.target.getAttribute('data-sat');
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+  const hue = sliders[0];
+  const brightness = sliders[1];
+  const saturation = sliders[2];
+
+  const bgColor = colorDivs[index].querySelector('h2').innerText;
+  console.log(bgColor);
+
+  let color = chroma(bgColor)
+    .set('hsl.s', saturation.value)
+    .set('hsl.l', brightness.value)
+    .set('hsl.h', hue.value);
+
+  colorDivs[index].style.backgroundColor = color;
+
+  // //Colorize inputs/sliders
+  // colorizeSliders(color, hue, brightness, saturation);
+}
+
 randomColors();
