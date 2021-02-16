3dd3bd602566e5f06371622479e8146e70c08f62
james reantillo <james.reantillo@gmail.com>
2021-02-15 19:11:44 -0800

    add to clipboard

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const colorDivs = document.querySelectorAll('.color');
 const generateBtn = document.querySelector('.generate');
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
+const popup = document.querySelector('.copy-container');
 let initialColors;
 
 //Event Listeners
@@ -13,6 +14,18 @@ colorDivs.forEach((div, index) => {
   div.addEventListener('change', () => {
     updateTextUI(index);
   });
+});
+
+currentHexes.forEach((hex) => {
+  hex.addEventListener('click', () => {
+    copyToClipboard(hex);
+  });
+});
+
+popup.addEventListener('transitionend', () => {
+  const popupBox = popup.children[0];
+  popup.classList.remove('active');
+  popupBox.classList.remove('active');
 });
 
 //Functions
@@ -131,6 +144,18 @@ function resetInputs() {
       slider.value = brightValue;
     }
   });
+}
+function copyToClipboard(hex) {
+  const el = document.createElement('textarea');
+  el.value = hex.innerText;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  //Pop up animation
+  const popupBox = popup.children[0];
+  popup.classList.add('active');
+  popupBox.classList.add('active');
 }
 
 randomColors();
